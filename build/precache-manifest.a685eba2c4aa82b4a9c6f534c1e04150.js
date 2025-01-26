c35a7ce85d8a3178638c091c27a034c71bdca200
Omer <omer.aslam@asiaverify.com>
2025-01-26 20:16:17 +0500

    builf

---
+++ build/precache-manifest.a685eba2c4aa82b4a9c6f534c1e04150.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f0ee9f9d6840e5bf65278ffab0e962",
+    "url": "/3pl-frontend/index.html"
+  },
+  {
+    "revision": "36a7bee43b1482e33a05",
+    "url": "/3pl-frontend/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "6a13859f31aa49420392",
+    "url": "/3pl-frontend/static/css/main.5ee3f738.chunk.css"
+  },
+  {
+    "revision": "36a7bee43b1482e33a05",
+    "url": "/3pl-frontend/static/js/2.6e03d365.chunk.js"
+  },
+  {
+    "revision": "43fbabab77a5c27b80365c6f5ba6fa4e",
+    "url": "/3pl-frontend/static/js/2.6e03d365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8303fcfb8c4f89e3031f",
+    "url": "/3pl-frontend/static/js/3.0fef68eb.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/3pl-frontend/static/js/3.0fef68eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e707e6ed89a6cd5be07",
+    "url": "/3pl-frontend/static/js/4.d53586fc.chunk.js"
+  },
+  {
+    "revision": "b63e781656a7c21988295c1056f07347",
+    "url": "/3pl-frontend/static/js/4.d53586fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a13859f31aa49420392",
+    "url": "/3pl-frontend/static/js/main.bf34e8f6.chunk.js"
+  },
+  {
+    "revision": "feefcfa96284021451ec7e71a9f83892",
+    "url": "/3pl-frontend/static/js/main.bf34e8f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e70aa3fc70b9ef839ae9",
+    "url": "/3pl-frontend/static/js/runtime-main.e4ebd1cb.js"
+  },
+  {
+    "revision": "5f4908f6d3cd2c1420127976b3b03bac",
+    "url": "/3pl-frontend/static/media/A9.5f4908f6.jpg"
+  },
+  {
+    "revision": "30185acb1deceff89c252276451349b3",
+    "url": "/3pl-frontend/static/media/M10.30185acb.jpg"
+  },
+  {
+    "revision": "7f61a62502e64f538649235cebe5052f",
+    "url": "/3pl-frontend/static/media/avatar.7f61a625.jpg"
+  },
+  {
+    "revision": "3d9adc3284961ca61c53317d061743b2",
+    "url": "/3pl-frontend/static/media/flickfootball.3d9adc32.jpg"
+  },
+  {
+    "revision": "3fa183635123b5d03e85c80e2ac8924e",
+    "url": "/3pl-frontend/static/media/hair.3fa18363.jpg"
+  },
+  {
+    "revision": "bece793776389f6457c83d8388f554ed",
+    "url": "/3pl-frontend/static/media/login.bece7937.jpeg"
+  },
+  {
+    "revision": "720dac336cea5e439fac3dac1d4a7cd7",
+    "url": "/3pl-frontend/static/media/logo.720dac33.png"
+  },
+  {
+    "revision": "0cadad6d7eceafa61d7b5e64de6caf00",
+    "url": "/3pl-frontend/static/media/logodark.0cadad6d.png"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/3pl-frontend/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/3pl-frontend/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/3pl-frontend/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/3pl-frontend/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/3pl-frontend/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "d22dc3ae86e8a02ceea3b513227fa9af",
+    "url": "/3pl-frontend/static/media/stand.d22dc3ae.jpg"
+  }
+]);
